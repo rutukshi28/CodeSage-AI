425af7d5cea07370748cfb377ea9e61e8c269174
rutuja28 <rutukshirsagar2006@gmail.com>
2026-03-29 16:52:03 +0530

    updated API URL for deployment

--- frontend/codeSage-AI/src/services/api.js
+++ frontend/codeSage-AI/src/services/api.js
@@ -1,5 +1,5 @@
 export const reviewCodeAPI = async (code, language) => {
-  const res = await fetch("http://localhost:5000/api/review", {
+  const res = await fetch("https://codesage-ai-mmtc.onrender.com/api/review", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -14,7 +14,7 @@ export const reviewCodeAPI = async (code, language) => {
 };
 
 export const runCodeAPI = async (code, language) => {
-  const res = await fetch("http://localhost:5000/api/review/run", {
+  const res = await fetch("https://codesage-ai-mmtc.onrender.com/api/review/run", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
